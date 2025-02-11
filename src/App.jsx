1e25094044f53f70d70b7705543e888ee2a880a4
guimanso <gui.2015.mv@gmail.com>
2025-02-11 04:00:11 -0300

    feat: adicionado tela principal

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./commponents/Header";
+import Main from "./commponents/Main";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    );
+};
+
+export default App;
